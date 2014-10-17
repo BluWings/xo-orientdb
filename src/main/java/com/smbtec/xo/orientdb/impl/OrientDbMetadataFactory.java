5837be48ad8e816119388a82bd0892cd56f211dd
Lars Martin <lars.martin@smb-tec.com>
2014-10-17 22:27:04 +0200

    add license header

--- src/main/java/com/smbtec/xo/orientdb/impl/OrientDbMetadataFactory.java
+++ src/main/java/com/smbtec/xo/orientdb/impl/OrientDbMetadataFactory.java
@@ -1,3 +1,21 @@
+/*
+ * eXtended Objects - OrientDb Binding
+ *
+ * Copyright (C) 2014 SMB GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.smbtec.xo.orientdb.impl;
 
 import java.util.Map;
@@ -12,6 +30,7 @@ import com.buschmais.xo.spi.reflection.AnnotatedElement;
 import com.buschmais.xo.spi.reflection.AnnotatedMethod;
 import com.buschmais.xo.spi.reflection.AnnotatedType;
 import com.buschmais.xo.spi.reflection.PropertyMethod;
+
 import com.smbtec.xo.orientdb.impl.metadata.CollectionPropertyMetadata;
 import com.smbtec.xo.orientdb.impl.metadata.EdgeMetadata;
 import com.smbtec.xo.orientdb.impl.metadata.IndexedPropertyMetadata;
@@ -24,6 +43,7 @@ import com.smbtec.xo.tinkerpop.blueprints.api.annotation.Property;
 import com.smbtec.xo.tinkerpop.blueprints.api.annotation.Vertex;
 import com.smbtec.xo.tinkerpop.blueprints.api.annotation.Edge.Incoming;
 import com.smbtec.xo.tinkerpop.blueprints.api.annotation.Edge.Outgoing;
+
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Element;
 
